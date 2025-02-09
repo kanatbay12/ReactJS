c09d427540d19e5b3bfb459f1ca289d04ee252c2
kanatbay12 <kana7292@gmail.com>
2025-02-09 17:49:03 +0500

    Добавил Sidebar

--- first/src/App.jsx
+++ first/src/App.jsx
@@ -1,5 +1,3 @@
-import "./App.css";
-
 import Header from "./components/Header.jsx";
 import Content from "./components/Content.jsx";
 import Footer from "./components/Footer.jsx";
--- first/src/components/Content.jsx
+++ first/src/components/Content.jsx
@@ -1,8 +1,15 @@
 function Content() {
   return (
-    <>
-      <div className="pt-16">Content</div>
-    </>
+    <div className="flex">
+      <aside className="w-64 h-screen bg-gray-200 fixed top-0 left-0 pt-16">
+        {/* Сайдбар */}
+        <div className="p-4">Sidebar</div>
+      </aside>
+      <main className="ml-64 pt-16 flex-1 p-4">
+        {/* Основной контент */}
+        Content
+      </main>
+    </div>
   );
 }
 
